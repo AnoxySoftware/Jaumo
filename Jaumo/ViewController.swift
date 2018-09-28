4e317f95a5e8abf41ad621efc0cb689b07e1928b
Lefteris <AnoxySoftware@users.noreply.github.com>
2018-09-28 13:31:06 +0300

    Initial Commit

---
+++ Jaumo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jaumo
+//
+//  Created by Lefteris Haritou on 28/09/2018.
+//  Copyright © 2018 Lefteris Haritou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
